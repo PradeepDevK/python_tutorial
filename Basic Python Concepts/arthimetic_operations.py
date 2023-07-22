f19100ffa73291d0d875b420b2e1a3e99a15043c
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-22 23:00:58 +0530

    first commit

---
+++ Basic Python Concepts/arthimetic_operations.py
@@ -0,0 +1,15 @@
+print("addition", 100+2)
+
+print("subtract", 300-49)
+
+print("subtract", 5-10)
+
+print("multiply", 45*78)
+
+print("mixed", 4*6*(28+5))
+
+print("divide returns float", 200/2)
+
+print("divide returns integer", 200//2)
+
+print("multiply with floating number", 48.0/2)
