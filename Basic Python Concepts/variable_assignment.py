60455c167af1c9fae930a0d4979c95f45cbb5950
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-24 15:26:41 +0530

    variable assignment

---
+++ Basic Python Concepts/variable_assignment.py
@@ -0,0 +1,21 @@
+a = 200
+print(a)
+
+a = 10
+print(a) #value of the a temparory memory location has been modified
+
+name = "John"
+print(name)
+print(type(name))
+
+b = 4.5
+print(b)
+
+c = True
+print(type(c), c)
+
+price = 100
+print(type(price), price)
+
+price = "None"
+print(type(price), price) #python is a dynamical typed language
