195830a3acc54fdb67279363d0feb8b51e735b76
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-24 15:40:10 +0530

    comparison operators

---
+++ Basic Python Concepts/comparison_operators.py
@@ -0,0 +1,17 @@
+a=10
+print("equal to", a)
+
+#comparison operator
+
+b=20
+print(a==b)
+
+print(a!=b) #not equal to operator
+
+print(a>b) #greater than operator
+
+print(a<b) #lesser than operator
+
+print(a>=b) #greater than equal to operator
+
+print(a<=b) #lesser than equal to operator
