15f421c87be9de3f2a0c651b636b3318fc72dc1d
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-24 16:39:48 +0530

    accepting user input

---
+++ Basic Python Concepts/accepting_input.py
@@ -0,0 +1,13 @@
+a = input("Enter first number")
+b = input("Enter second number")
+print(type(a), type(b))
+
+a = int(a)
+b = int(b)
+
+print(type(a), type(b))
+
+c = int(input("Enter third number"))
+
+d = a + b + c
+print("Sum", d)
