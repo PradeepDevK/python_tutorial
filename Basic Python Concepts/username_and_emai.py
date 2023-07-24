4bf2d52f77eb2c230a1c5ce609b1eeedc32120ad
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-24 16:45:31 +0530

    accepting user input part2

---
+++ Basic Python Concepts/username_and_emai.py
@@ -0,0 +1,8 @@
+firstname = input("Enter your firstname : ")
+lastname = input("Enter your lastname : ")
+
+username = firstname + lastname
+print("username", username)
+
+email = username + "@gmail.com"
+print("email", email)
