e552f136396e2acc016ef81152ac7c940b1e6a78
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-25 11:00:49 +0530

    f strings

---
+++ Basic Python Concepts/f_Strings.py
@@ -0,0 +1,7 @@
+name = "John"
+
+print(f"Hello {name}")
+
+a = 10
+b = 20
+print(f"Sum of numbers is  { a + b }")
--- Basic Python Concepts/simple_interest_calc.py
+++ Basic Python Concepts/simple_interest_calc.py
@@ -5,4 +5,8 @@ years = float(input("Enter the period in years : "))
 rate = float(input("Enter rate of inerest : "))
 
 interest = principal * years * rate / 100
-print("Simple Interest Calc :", str(interest))
+#print("Simple Interest Calc :", str(interest))
+
+#print("Simple Interest on the principal amount", principal, "For a actual period of", years, "years at the rate of", str(rate) + "% is $", interest)
+
+print(f"Simple Interest on the principal amount {principal}, For a actual period of {years} years at the rate of {rate}% is ${interest}")
