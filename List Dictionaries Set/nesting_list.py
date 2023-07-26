1fa9644d65e8f63dde91d875ea512c70373997a5
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-26 12:47:47 +0530

    nested list

---
+++ List Dictionaries Set/nesting_list.py
@@ -0,0 +1,17 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+
+b = [1, 2, [3, 4, 5], 6, 7, 8, [9, 10], [11, 12, [13, 14]]]
+
+print(a)
+
+print(b)
+
+print(b[1])
+
+print(b[2])
+
+print(b[2][1]) #access nested list
+
+print(b[6][1])
+
+print(b[7][2][1])
