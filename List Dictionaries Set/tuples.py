7447402f17e301eaaeab275c7ffa380fe4832654
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-26 13:23:29 +0530

    tuples

---
+++ List Dictionaries Set/tuples.py
@@ -0,0 +1,14 @@
+#Tuples
+fruits = ("apple", "orange", "mango", "watermelon")
+print(fruits)
+
+print(fruits[1])
+
+#imutability
+#fruits[1] = "Peach" this list mutability doesn't work in tuples
+
+print(fruits[1:3]) #tuples slicing
+
+print(fruits[-1]) #negative index
+
+#as comparing to lists, tuples works faster and data cannot be changed
