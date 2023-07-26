4e7b272cc9c8e615a6f6217b36c32df11a952622
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-26 11:27:11 +0530

    storing list

---
+++ List Dictionaries Set/storing_list.py
@@ -0,0 +1,16 @@
+people = ['Jhon', 'Rob', 'Mike']
+print(people)
+
+number = [1, 2, 3, 4]
+print(number)
+
+floating = [1.5, 2.5, 3.5, 5.5, 4.5]
+print(floating)
+
+items = ['Computer', 10, 4.5, True, 'Mouse', 4.6] #mixed data types
+print(items)
+print(items[3])
+
+a = [1, 2]
+b = [1, 2]
+print(a == b)
