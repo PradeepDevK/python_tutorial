629763c92ddffc95918d2b31f3bae5d8702955d1
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-26 11:42:48 +0530

    in and not in operators list

---
+++ List Dictionaries Set/in_not_in_operator.py
@@ -0,0 +1,8 @@
+fruits = ['Apple', 'Mango', 'Peach', 'Orange', 'Watermelon', 'Grape']
+print(fruits)
+
+print('Apple' in fruits)
+print('Tomato' in fruits)
+
+print('PineApple' not in fruits)
+print('Mango' not in fruits)
