ba1ade2bcea255bf0c54ff0dfc63db3bbf8f99e5
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-26 13:39:38 +0530

    dictionary using dict()

---
+++ List Dictionaries Set/dictionary_dict().py
@@ -0,0 +1,15 @@
+people = dict(
+    john = 32,
+    rob = 40,
+    tim = 20
+)
+
+print(people)
+
+people["mike"] = 30 #adding new key-value to dictionary
+print(people)
+
+#delete key-value
+del people["mike"]
+print(people)
+
