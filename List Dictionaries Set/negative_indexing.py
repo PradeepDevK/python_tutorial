b065e09bbcb35d0d5c79c1ff15fd291a16366a12
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-26 11:31:50 +0530

    negative indexing

---
+++ List Dictionaries Set/negative_indexing.py
@@ -0,0 +1,11 @@
+fruits = ['Apple', 'Mango', 'Peach', 'Orange', 'Watermelon', 'Grape']
+#+ve        0        1         2        3         4           5
+#-ve       -6       -5        -4       -3        -2          -1
+
+print(fruits)
+
+print(fruits[2]) #print Peach
+
+#Negative index
+print(fruits[-1]) #accessing last index
+print(fruits[-4])
