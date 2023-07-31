04738a5678fa1fe37b8de109ed14652985f4ec7f
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-07-31 21:13:53 +0530

    get method

---
+++ List Dictionaries Set/get_method.py
@@ -0,0 +1,14 @@
+people = {
+    "John": 32,
+    "Rob": 40,
+    "Tim": 20
+}
+
+#get method
+print(people.get("Rob"))
+
+print(people["Rob"])
+
+#print(people["Jim"]) //it will throw an that's why we need get method
+
+print(people.get("Jim"))
