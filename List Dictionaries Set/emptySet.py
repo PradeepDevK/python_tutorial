3e6f7bd9b3676baaa6f371b7c96488dfdff0ecda
pradeeprajkk <pradeep.raj@accionlabs.com>
2023-08-05 23:15:59 +0530

    set

---
+++ List Dictionaries Set/emptySet.py
@@ -0,0 +1,12 @@
+names = { 'John', 'Rob', 'Mike', 'Jhon' }
+print(names)
+
+s = { 'John', 12, 4.5, True }
+print(s)
+
+s1 = {}
+print(type(s1))
+
+#empty set
+s2 = set()
+print(type(s2))
---
+++ List Dictionaries Set/set_creating_set.py
@@ -0,0 +1,12 @@
+numbers = set([1, 2, 3, 4, 5, 6])
+print(numbers)
+
+#another way of creating sets
+num1 = { 1, 2, 3, 4, 5, 6 } #note, this doesn't dictionary since it don't have key-value pair
+print(num1)
+
+num2 = { 1, 2, 3, 4, 3, 5}
+print(num2) #in sets duplicate will be avoided
+
+num3 = { 1, 3, 4, 5, 2}
+print(num3) #sets is unordered, we can't use index to get the value
